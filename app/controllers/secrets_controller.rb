bf5d3279442f28b8f491f67264de7474975881fb
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-03-14 19:52:18 +0600

    added post creation and submission

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,29 @@
 class SecretsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @secrets = Secret.all.order('created_at DESC')
+  end
+
+  def new
+    @secret = Secret.new
+  end
+
+  def create
+    @secrets = Secret.all.order('created_at DESC')
+    @secret = current_user.secrets.build(secret_params)
+
+    respond_to do |format|
+      if @secret.save
+        format.html { redirect_to root_path, notice: "Secret was successfully created." }
+      else
+
+        format.html { render 'new', status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def secret_params
+    params.require(:secret).permit(:title, :body)
   end
 end
--- app/models/secret.rb
+++ app/models/secret.rb
@@ -2,5 +2,5 @@ class Secret < ApplicationRecord
   belongs_to :user
   validates :title, :body, presence: true
   validates :title, length: { in: 3..50}
-  validates :body, length: { minimum: 30}
+  validates :body, length: { minimum: 10}
 end
